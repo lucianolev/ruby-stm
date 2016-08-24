93a18d80b382c896af7dbb16834862400d9f1ad4
Luciano Leveroni <lleveroni@eryx.co>
2016-08-24 20:33:33 -0300

    Custom Atomic methods refactor
    Remove unneeded custom atomic methods in Array and Hash. Move Object atomic instance_variable_get and instance_variable_set methods to main object.rb file.

--- src/ruby_core_ext/object.rb
+++ src/ruby_core_ext/object.rb
@@ -44,6 +44,16 @@ class Object
     end
   end
 
+  define_method(:instance_variable_set.to_atomic_method_name) do |var_name, value|
+    # puts "Var #{var_name.inspect} set to value #{value.inspect}"
+    working_copy.instance_variable_set(var_name, value)
+  end
+
+  define_method(:instance_variable_get.to_atomic_method_name) do |var_name|
+    # puts "Var #{var_name.inspect} get"
+    working_copy.instance_variable_get(var_name)
+  end
+
   private
 
   def class_of_method_def(method_name)
