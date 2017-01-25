7507823145fb6482289e838aba6f103bcd577c1f
Luciano Leveroni <lleveroni@eryx.co>
2017-01-24 23:13:27 -0300

    Debug messages on instance variables access through working copy.

--- src/ruby_core_ext/object.rb
+++ src/ruby_core_ext/object.rb
@@ -24,12 +24,12 @@ class Object
   end
 
   define_method(:instance_variable_set.to_atomic_method_name) do |var_name, value|
-    # puts "Var #{var_name.inspect} set to value #{value.inspect}"
+    puts "DEBUG: Var #{var_name.inspect} set to value #{value.inspect} in working copy"
     working_copy.instance_variable_set(var_name, value)
   end
 
   define_method(:instance_variable_get.to_atomic_method_name) do |var_name|
-    # puts "Var #{var_name.inspect} get"
+    puts "DEBUG: Var #{var_name.inspect} get from working copy"
     working_copy.instance_variable_get(var_name)
   end
 
