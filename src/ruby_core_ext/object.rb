3bc01f1ec9a444d7a9dc4e3018635a6bdddbaa52
Luciano Leveroni <lleveroni@eryx.co>
2017-01-31 16:05:21 -0300

    Small refactor of Object#method_missing

--- src/ruby_core_ext/object.rb
+++ src/ruby_core_ext/object.rb
@@ -8,12 +8,7 @@ require_relative 'internal_state_primitives/object'
 class Object
   def method_missing(method_name, *args, &block)
     if method_name.is_an_atomic_method_name?
-      original_method_name = method_name.to_nonatomic_method_name
-      assert_original_method_is_defined(original_method_name)
-      class_of_method = class_of_method_def(original_method_name)
-      puts "DEBUG: Defining atomic method #{class_of_method}##{method_name} (obj #{self})"
-      class_of_method.define_atomic_method(original_method_name)
-      resend_atomic_method(method_name, args, block)
+      define_and_dispatch_atomic_variant(method_name, args, block)
     else
       super
     end
@@ -34,6 +29,15 @@ class Object
   end
 
   private
+
+  def define_and_dispatch_atomic_variant(method_name, args, block)
+    original_method_name = method_name.to_nonatomic_method_name
+    assert_original_method_is_defined(original_method_name)
+    class_of_method = class_of_method_def(original_method_name)
+    puts "DEBUG: Defining atomic method #{class_of_method}##{method_name} (obj #{self})"
+    class_of_method.define_atomic_method(original_method_name)
+    resend_atomic_method(method_name, args, block)
+  end
 
   def class_of_method_def(method_name)
     # Singleton methods are defined in singleton's class of the object instead of object's class.
